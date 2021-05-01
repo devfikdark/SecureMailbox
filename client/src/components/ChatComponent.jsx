269c3eedb0346fd0facae58da276e7c19b81e9dc
zubayerhimel <zhimel007@gmail.com>
2021-05-02 00:12:28 +0600

    updated chat page

--- client/src/components/ChatComponent.jsx
+++ client/src/components/ChatComponent.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 import { Box, Card, CardContent, IconButton, TextField, Typography } from "@material-ui/core";
+import { deepPurple, blue } from "@material-ui/core/colors";
 import SendIcon from "@material-ui/icons/Send";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
+  messages: {
+    height: "57vh",
+    backgroundColor: "",
+  },
+  receivedMessage: {
+    padding: 10,
+    borderRadius: 15,
+    backgroundColor: deepPurple[50],
+  },
+  sentMessage: {
+    padding: 10,
+    borderRadius: 15,
+    backgroundColor: blue[50],
+  },
   card: {
     position: "relative",
   },
@@ -11,16 +26,12 @@ const useStyles = makeStyles((theme) => ({
     position: "absolute",
     bottom: 0,
     left: 0,
-    width: "31vw",
+    width: "38em",
   },
   messageButton: {
     position: "absolute",
     bottom: 8,
     right: 10,
-  },
-  messages: {
-    height: "57vh",
-    backgroundColor: "purple",
   },
 }));
 
@@ -34,15 +45,16 @@ function ChatComponent() {
             <Typography>Username</Typography>
           </Box>
           <Box className={classes.messages}>
-            <Box display="flex" justifyContent="start">
-              lsldkfjlksdfj
+            <Box display="flex" justifyContent="flex-start" className={classes.receivedMessage} mb={1}>
+              hello there, how are you? hello there, how are you? hello there, how are you? hello there, how are you? hello there, how are you? hello there, how are you? hello there, how are you?
+              hello there, how are you?
             </Box>
-            <Box display="flex" justifyContent="end">
-              lsldkfjlksdfj
+            <Box display="flex" justifyContent="flex-end" className={classes.sentMessage} mb={1}>
+              I am fine. how is it going?
             </Box>
           </Box>
           <TextField variant="outlined" placeholder="Type your message" className={classes.messageField} />
-          <IconButton className={classes.messageButton} size="large" color="secondary">
+          <IconButton className={classes.messageButton} color="secondary">
             <SendIcon />
           </IconButton>
         </CardContent>
