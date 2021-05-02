7f7e5ce97e28b508ea1f46b6726eed8f597cca31
zubayerhimel <zhimel007@gmail.com>
2021-05-02 22:01:10 +0600

    added notification list page

--- client/src/pages/NotificationListPage.jsx
+++ client/src/pages/NotificationListPage.jsx
@@ -1,9 +1,34 @@
 import React from "react";
+import { Box, Container, Grid, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { blueGrey } from "@material-ui/core/colors";
+
+const useStyles = makeStyles((theme) => ({
+  notificationPanel: {
+    backgroundColor: blueGrey[50],
+    borderRadius: 10,
+  },
+}));
 
 function NotificationListPage() {
+  const classes = useStyles();
   return (
     <div>
-      <h1>Notification list</h1>
+      <Container maxWidth="md">
+        <Box my={4} display="flex" justifyContent="center">
+          <Typography variant="h4">Notification List</Typography>
+        </Box>
+        <Box display="flex" justifyContent="flex-start" className={classes.notificationPanel} p={2} mb={1}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Typography variant="subtitle1">Notification title</Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="body-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum sed eos iusto debitis odio nisi quam voluptatem eius voluptas nulla!</Typography>
+            </Grid>
+          </Grid>
+        </Box>
+      </Container>
     </div>
   );
 }
