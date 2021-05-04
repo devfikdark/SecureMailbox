d9500b0d5f30ae429749da3b79b1e3b8e282c261
zubayerhimel <zhimel007@gmail.com>
2021-05-04 23:02:26 +0600

    integrated notification list

--- client/src/pages/NotificationListPage.jsx
+++ client/src/pages/NotificationListPage.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Box, Container, Grid, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { blueGrey } from "@material-ui/core/colors";
+import moment from "moment";
+import axios from "axios";
 
 const useStyles = makeStyles((theme) => ({
   notificationPanel: {
@@ -12,23 +14,45 @@ const useStyles = makeStyles((theme) => ({
 
 function NotificationListPage() {
   const classes = useStyles();
+
+  const [notificationList, setNotificationList] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/notifications", {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      })
+      .then((res) => {
+        if (res.data.status === "ok") {
+          console.log(res.data.data);
+          setNotificationList(res.data.data);
+        } else {
+          Notification("Error", `${res.data.message}`, "error");
+        }
+      })
+      .catch((err) => Notification("Error", "Your session has expired. Please login again", "error"));
+  }, []);
   return (
     <div>
       <Container maxWidth="md">
         <Box my={4} display="flex" justifyContent="center">
           <Typography variant="h4">Notification List</Typography>
         </Box>
-        <Box display="flex" justifyContent="flex-start" className={classes.notificationPanel} p={2} mb={1}>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <Typography variant="subtitle1">Notification title</Typography>
-              <Typography variant="caption">Sun 22, 2021</Typography>
+        {notificationList.map((el) => (
+          <Box display="flex" justifyContent="flex-start" className={classes.notificationPanel} p={2} mb={1}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <Typography variant="subtitle1">{el.subject}</Typography>
+                <Typography variant="caption">{moment(el.createAt).fromNow()}</Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="body2">{el.message}</Typography>
+              </Grid>
             </Grid>
-            <Grid item xs={12}>
-              <Typography variant="body1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum sed eos iusto debitis odio nisi quam voluptatem eius voluptas nulla!</Typography>
-            </Grid>
-          </Grid>
-        </Box>
+          </Box>
+        ))}
       </Container>
     </div>
   );
