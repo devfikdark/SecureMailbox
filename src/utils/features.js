436a56938842132af0fb907e1cbb07a276b4d969
jinnatul <morolswediu@gmail.com>
2024-06-28 16:24:56 +0600

    feat: change socket

--- src/utils/features.js
+++ src/utils/features.js
@@ -1,7 +1,7 @@
 import io from "socket.io-client";
 import feathers from "@feathersjs/client";
 
-const socket = io("http://localhost:5000");
+const socket = io(process.env.REACT_APP_SOCKET_URL);
 const app = feathers();
 
 app.configure(feathers.socketio(socket));
